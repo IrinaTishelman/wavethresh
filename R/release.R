d591594ea61affeaed7d6d3ec978f1e4712051dd
Martin Maechler <maechler@stat.math.ethz.ch>
2009-01-10 00:00:00 +0000

    version 2.2-11

--- R/release.R
+++ R/release.R
@@ -1,12 +1,12 @@
 wvrelease <- function(do.cat = interactive())
 {
-    wv <- list(major = 2.2, R.minor = 10)
+    wv <- list(major = 2.2, R.minor = 11)
     ##                 ---            --
     if(do.cat)
 	cat("S/R wavelet software, release ", wv$major,", installed\n",
 	    "Copyright Guy Nason 1993\n",
 	    "R version ", wv$major,"-", wv$R.minor,
-	    ": Arne Kovac 1997; Martin Maechler, 1999-2008\n", sep="")
+	    ": Arne Kovac 1997; Martin Maechler, 1999-2009\n", sep="")
 ### Don't forget the                             ^^^^^^^^^  year range  !
     invisible(wv)
 }
